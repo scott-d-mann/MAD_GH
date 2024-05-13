88b1cc4a8415216bda1c72c826623b300933ff0b
Scott Mann <s.mann@latrobe.edu.au>
2024-05-13 16:38:20 +1000

    Test Activity2

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
